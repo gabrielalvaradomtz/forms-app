17b7f2cf638b26f9b121b86f727143eb3bda5cb5
Gabriela Lizbeth Alvarado Martinez <gabrielalizbethalvaradomartinez@MacBook-Pro-de-Gabriela.local>
2024-07-25 11:57:49 -0600

    Se crea primer formulario reactivo.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { SideMenuComponent } from './componentes/side-menu/side-menu.component';
+
+
+@NgModule({
+  declarations: [
+    SideMenuComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedRoutingModule,
+    RouterModule
+  ],
+  exports: [
+    SideMenuComponent
+  ]
+})
+export class SharedModule { }
