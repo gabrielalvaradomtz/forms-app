c72b17b1e15c0e170537346b9872bc5e4b9ef9a3
Gabriela Lizbeth Alvarado Martinez <gabrielalizbethalvaradomartinez@MacBook-Pro-de-Gabriela.local>
2024-08-14 10:20:22 -0600

    Creación de formulario de registro.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
+  
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {
+    // TODO: Obtener validación de un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+  }
 
 }
